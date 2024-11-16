c78d8450e4f7ab6256e2758c504189cdab5d8235
Kieran Emery <kizwiz@hotmail.co.uk>
2024-11-16 21:03:04 +0000

    Updated VehicleDetails model with public properties with each representing a specific piece of information about a vehicle. Updated IVehicleService interface with an asynchronous method that defines how the service retrieves detailed vehicle information, including the MOT status, based on the registration number.

--- MotChecker/Models/VehicleDetails.cs
+++ MotChecker/Models/VehicleDetails.cs
@@ -5,5 +5,35 @@
     /// </summary>
     public class VehicleDetails
     {
+        /// <summary>
+        /// The vehicle's registration number
+        /// </summary>
+        public string Registration { get; set; } = string.Empty;
+        
+        /// <summary>
+        /// The manufacturer of the vehicle 
+        /// </summary>
+        public string Make { get; set; } = string.Empty;
+        
+        /// <summary>
+        /// The model name/number of the vehicle
+        /// </summary>
+        public string Model { get; set; } = string.Empty;
+        
+        /// <summary>
+        /// The colour of the vehicle
+        /// </summary>
+        public string Colour { get; set; } = string.Empty;
+        
+        /// <summary>
+        /// The date when the current MOT certificate expires
+        /// </summary>
+        public DateTime NotExpiryDate { get; set; }
+
+        /// <summary>
+        /// The recorded mileage at the most recent MOT test
+        /// </summary>
+        public int MileageAtLastMot { get; set; }
+
     }
 }
--- MotChecker/Services/IVehicleService.cs
+++ MotChecker/Services/IVehicleService.cs
@@ -5,5 +5,11 @@
     /// </summary>
     public interface IVehicleService
     {
+        /// <summary>
+        /// Retrieves detailed vehicle information including MOT status by registration number
+        /// </summary>
+        /// <param name="registration">The vehicle registration number to query</param>
+        /// <returns>A task containing the vehicle details if found</returns>
+        Task<Models.VehicleDetails> GetVehicleDetailsAsync(string registration);
     }
 }
