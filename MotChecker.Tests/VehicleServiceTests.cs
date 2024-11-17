d2588ce1bb5819e42eda2cce5d82e90bb00a3ca1
Kieran Emery <kizwiz@hotmail.co.uk>
2024-11-17 20:44:30 +0000

    Add comprehensive test suite, with a VehicleServiceTests class to test the service calls. Updates the csproj file with the necessary test packages.

---
+++ MotChecker.Tests/VehicleServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using Moq;
+using FluentAssertions;
+using MotChecker.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace MotChecker.Tests
+{
+    public class VehicleServiceTests
+    {
+        private readonly Mock<ILogger<MockVehicleService>> _loggerMock;
+        private readonly IMemoryCache _memoryCache;
+
+        public VehicleServiceTests()
+        {
+            _loggerMock = new Mock<ILogger<MockVehicleService>>();
+            _memoryCache = new MemoryCache(new MemoryCacheOptions());
+        }
+
+        [Fact]
+        public async Task GetVehicleDetailsAsync_Should_Return_Vehicle_Details()
+        {
+            // Arrange
+            var service = new MockVehicleService(_loggerMock.Object);
+            var registration = "AB12CDE";
+
+            // Act
+            var result = await service.GetVehicleDetailsAsync(registration);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Registration.Should().Be(registration);
+            result.Make.Should().NotBeNullOrEmpty();
+            result.Model.Should().NotBeNullOrEmpty();
+            result.Colour.Should().NotBeNullOrEmpty();
+            result.MotExpiryDate.Should().BeAfter(DateTime.Now);
+            result.MileageAtLastMot.Should().BeGreaterThan(0);
+        }
+    }
+}
