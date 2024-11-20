994144392e2d38bc50cc6cfb68332d1f8347260e
Kieran Emery <kizwiz@hotmail.co.uk>
2024-11-20 11:54:32 +0000

    Add Json property names to VehicleDetails.

--- MotChecker/Models/VehicleDetails.cs
+++ MotChecker/Models/VehicleDetails.cs
@@ -1,4 +1,6 @@
-﻿namespace MotChecker.Models
+﻿using System.Text.Json.Serialization;
+
+namespace MotChecker.Models
 {
     /// <summary>
     /// Represents detailed information about a vehicle and its MOT status
@@ -8,31 +10,37 @@
         /// <summary>
         /// The vehicle's registration number
         /// </summary>
+        [JsonPropertyName("registration")]
         public string Registration { get; set; } = string.Empty;
-        
+
         /// <summary>
         /// The manufacturer of the vehicle 
         /// </summary>
+        [JsonPropertyName("make")]
         public string Make { get; set; } = string.Empty;
-        
+
         /// <summary>
         /// The model name/number of the vehicle
         /// </summary>
+        [JsonPropertyName("model")]
         public string Model { get; set; } = string.Empty;
-        
+
         /// <summary>
         /// The colour of the vehicle
         /// </summary>
+        [JsonPropertyName("colour")]
         public string Colour { get; set; } = string.Empty;
-        
+
         /// <summary>
         /// The date when the current MOT certificate expires
         /// </summary>
+        [JsonPropertyName("motExpiryDate")]
         public DateTime MotExpiryDate { get; set; }
 
         /// <summary>
         /// The recorded mileage at the most recent MOT test
         /// </summary>
+        [JsonPropertyName("mileageAtLastMot")]
         public int MileageAtLastMot { get; set; }
 
     }
