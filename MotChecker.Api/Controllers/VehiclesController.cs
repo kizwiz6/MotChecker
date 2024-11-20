fc0b9ad8d26511c2db40e7d9f52842ff0c72c3de
Kieran Emery <kizwiz@hotmail.co.uk>
2024-11-20 11:28:53 +0000

    Improve documentation. - Add comprehensive XML comments to DvsaApiProxy - Add XML comments to VehiclesController - Add XML comments to VehicleDetails model - Add parameter descriptions - Add exception documentation - Add response code documentation

--- MotChecker.Api/Controllers/VehiclesController.cs
+++ MotChecker.Api/Controllers/VehiclesController.cs
@@ -4,17 +4,32 @@ using MotChecker.Models;
 
 namespace MotChecker.Api.Controllers
 {
+    // VehiclesController.cs
+    /// <summary>
+    /// API Controller for handling vehicle information requests
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class VehiclesController : ControllerBase
     {
         private readonly DvsaApiProxy _dvsaApi;
 
+        /// <summary>
+        /// Initialises a new instance of the VehiclesController
+        /// </summary>
+        /// <param name="dvsaApi">DVSA API proxy service</param>
         public VehiclesController(DvsaApiProxy dvsaApi)
         {
             _dvsaApi = dvsaApi;
         }
 
+        /// <summary>
+        /// Retrieves vehicle details by registration number
+        /// </summary>
+        /// <param name="registration">Vehicle registration number</param>
+        /// <returns>Vehicle details if found</returns>
+        /// <response code="200">Returns the vehicle details</response>
+        /// <response code="500">If an error occurs during processing</response>
         [HttpGet("{registration}")]
         [Produces("application/json")]
         public async Task<ActionResult<VehicleDetails>> Get(string registration)
--- MotChecker.Api/Models/VehicleDetails.cs
+++ MotChecker.Api/Models/VehicleDetails.cs
@@ -2,18 +2,44 @@
 
 namespace MotChecker.Api.Models
 {
+    /// <summary>
+    /// Represents detailed information about a vehicle
+    /// </summary>
     public class VehicleDetails
     {
+        /// <summary>
+        /// The vehicle's registration number
+        /// </summary>
         [JsonPropertyName("registration")]
         public string Registration { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The manufacturer of the vehicle
+        /// </summary>
         [JsonPropertyName("make")]
         public string Make { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The model of the vehicle
+        /// </summary>
         [JsonPropertyName("model")]
         public string Model { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The primary color of the vehicle
+        /// </summary>
         [JsonPropertyName("primaryColour")]
         public string Colour { get; set; } = string.Empty;
         [JsonPropertyName("expiryDate")]
+
+        /// <summary>
+        /// The expiry date of the current MOT certificate
+        /// </summary>
         public DateTime MotExpiryDate { get; set; }
+
+        /// <summary>
+        /// The recorded mileage at the last MOT test
+        /// </summary>
         [JsonPropertyName("odometerValue")]
         public int MileageAtLastMot { get; set; }
     }
